7447edc8538f894f3e959e361bcdc4641ca2c3c8
PedroAguirreArias01 <20175260+PedroAguirreArias01@users.noreply.github.com>
2019-08-13 11:45:14 -0500

    creacion del proyecto

---
+++ src/main/java/com/battery/center/BatteryCenterApplication.java
@@ -0,0 +1,13 @@
+package com.battery.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatteryCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatteryCenterApplication.class, args);
+	}
+
+}
